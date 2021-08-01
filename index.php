1f74c8a487c22f75402bc70866654409c9c54b94
Marcelo Henrique <marcelohphn@gmail.com>
2021-08-01 20:47:36 -0300

    Instalação do wordpress

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/app.css">
-    <title>Starter Wordpress</title>
-</head>
-<body>
-    <div class="container">
-        <h1>Hello Base Teme</h1>
-        <button  class="btn btn-primary">Clique</button>
-    </div>
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-    <script  src="./assets/js/app.js"></script>
-</body>
-</html>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
